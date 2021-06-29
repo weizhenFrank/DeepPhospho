ec236a58330309203c178fdc5eab091ba00386d8
gureann <lourh@shanghaitech.edu.cn>
2021-06-29 14:31:53 +0800

    Update scripts help messages

--- build_spec_lib.py
+++ build_spec_lib.py
@@ -5,14 +5,15 @@ from deep_phospho import proteomics_utils as prot_utils
 from deep_phospho.model_utils.logger import setup_logger
 
 HelpMSG = '''
-This script have two usages and both of them build a ready to use spectral library with Spectronaut format
+This script have two usages and both of them will build a Spectronaut-compatible spectral library
 
 Usage 1: python build_spec_lib.py build -i pathA -r pathB -o pathC
-    this will read DeepPhospho predicted ion intensity from pathA and RT from pathB, then generated a library and output to pathC
+    this will read DeepPhospho predicted ion intensity (in JSON format) from pathA and RT (in txt format) from pathB, 
+    then generated a library and output to pathC
 
 Usage 2: python build_spec_lib.py merge -l pathA pathB ... -o pathO
     this will merge all passed libraries to one and output to pathO
-    [notice] the first library will be used as the major one
+    [notice] the first library will be used as the major one (i.e. spectra and RT in first library will be kept in priority)
 
 For more information, please visit our repository
 [DeepPhospho repository] https://github.com/weizhenFrank/DeepPhospho
@@ -39,7 +40,7 @@ if __name__ == '__main__':
     parser_merge.set_defaults(action='merge')
     # lib
     parser_merge.add_argument('-l', '--lib', metavar='path', type=str, nargs='*', required=True,
-                              help='Path of one or more library files')
+                              help='Paths of two or more library files')
     # output
     parser_merge.add_argument('-o', '--output', metavar='path', type=str, required=True,
                               help='Output path of merged library')
@@ -60,7 +61,7 @@ if __name__ == '__main__':
         logger = setup_logger('Merge library', None)
         all_libs = args.lib
         if len(all_libs) < 2:
-            raise ValueError(f'To merge libraries, the number of input libraries should >= 2')
+            raise ValueError(f'To merge libraries, the number of input libraries should be more than or equal to 2')
         path = prot_utils.gen_dp_lib.merge_lib(
             main_lib_path=all_libs[0],
             add_libs_path=dict(enumerate(all_libs[1:], 2)),
--- generate_dataset.py
+++ generate_dataset.py
@@ -3,13 +3,13 @@ import argparse
 from deep_phospho import proteomics_utils as prot_utils
 
 HelpMSG = '''
-This script have two usages to transform data to DeepPhospho training or prediction data
+This script will transform datasets from some supported sources to DeepPhospho compatible training and prediction input format
 
 Usage 1: python generate_dataset.py train -f pathA -t file_type -o dirA
-    this will transform the input train file with format (file_type) to DeepPhospho training data format and output to directory dirA
+    this will transform the input file in "pathA" with format "file_type" to DeepPhospho training data format and output to directory "dirA"
 
 Usage 2: python generate_dataset.py pred -f pathA -t file_type -o dirA
-    this will transform the input prediction file with format (file_type) to DeepPhospho prediction input format and output to directory dirA
+    this will transform the input file in "pathA" with format "file_type" to DeepPhospho prediction input format and output to directory "dirA"
 
 For more information, please visit our repository
 [DeepPhospho repository] https://github.com/weizhenFrank/DeepPhospho
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser_train.set_defaults(action='train')
     # file
     parser_train.add_argument('-f', '--file', metavar='path', type=str, required=True,
-                              help='Path of training data')
+                              help='Path of dataset for training')
     # type
     parser_train.add_argument('-t', '--type', metavar='str', type=str, required=True,
                               help='''The format (or source) of input file, and the valid values are listed as below:
@@ -39,14 +39,18 @@ if __name__ == '__main__':
     parser_pred.set_defaults(action='pred')
     # file
     parser_pred.add_argument('-f', '--file', metavar='path', type=str, required=True,
-                             help='Path of prediction data')
+                             help='''File contains under-predicted peptide precursors may has multi-source:
+I. spectral library from Spectronaut
+II. search result from Spectronaut
+III. msms.txt or evidence.txt from MaxQuant
+IV. any tab-separated file with two columns "sequence" and "charge"''')
     # type
     parser_pred.add_argument('-t', '--type', metavar='str', type=str, required=True,
-                             help='''The prediction data can have multi format:
+                             help='''The source of prediction input file or peptide format
 I. "SNLib" for Spectronaut library
 II. "SNResult" for Spectronaut result
 III. "MQ1.5" or "MQ1.6" for msms.txt/evidence.txt from MaxQuant version <= 1.5 or >= 1.6
-IV. any tab-separated file with two columns "sequence" and "charge. And for "sequence" column, the modified peptides in the following format are valid
+IV. for peptide list file, the modified peptides in the following format are valid
     a. "PepSN13" is Spectronaut 13+ peptide format like _[Acetyl (Protein N-term)]M[Oxidation (M)]LSLS[Phospho (STY)]PLK_
     b. "PepMQ1.5" is MaxQuant 1.5- peptide format like _(ac)GS(ph)QDM(ox)GS(ph)PLRET(ph)RK_
     c. "PepMQ1.6" is MaxQuant 1.6+ peptide format like _(Acetyl (Protein N-term))TM(Oxidation (M))DKS(Phospho (STY))ELVQK_
